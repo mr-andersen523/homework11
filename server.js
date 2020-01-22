a0768c8724f197452eb5ce2517408c31efea9e2d
mr andersen <chasea523@gmail.com>
2020-01-21 20:00:12 -0800

    add listener, js files, etc

--- server.js
+++ server.js
@@ -3,3 +3,19 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//  js files
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// use public folder
+app.use(express.static("public"));
+
+// listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
